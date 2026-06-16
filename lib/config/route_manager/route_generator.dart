7a4280865a948ab5e9c204f78719376812cf9e62
sarah elkholy <saraelkholyarmy03@gmail.com>
2026-06-16 22:45:40 +0300

    - added app sections icons - added app sections labels - finished bottom nav bar - created home,orders and profile screens

--- lib/config/route_manager/route_generator.dart
+++ lib/config/route_manager/route_generator.dart
@@ -1,5 +1,6 @@
 import 'package:flower_driver/config/route_manager/routes.dart';
 import 'package:flower_driver/core/helpers/custom_logger.dart';
+import 'package:flower_driver/core/shared_widgets/custom_bottom_nav.dart';
 import 'package:flower_driver/core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import '../../core/localization/l10n/app_localizations.dart';
@@ -12,6 +13,16 @@ abstract class RouteGenerator {
         /// Splash Screen
         case Routes.splashRoute:
           return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+        case Routes.bottomNavBarRoute:
+          final args = settings.arguments as Map<String, dynamic>?;
+
+          return MaterialPageRoute(
+            builder: (_) => CustomBottomNavBar(
+              initialIndex: args?['initialIndex'] ?? 0,
+              categoryIndex: args?['categoryIndex'] ?? 0,
+            ),
+          );
 
         /// Default
         default:
--- lib/features/auth/api/auth_api_client.dart
+++ lib/features/auth/api/auth_api_client.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/http.dart';
+import 'package:retrofit/error_logger.dart';
 
 part 'auth_api_client.g.dart';
 
