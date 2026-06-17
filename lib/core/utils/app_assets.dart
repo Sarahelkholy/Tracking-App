a898674be42a8a4f9527ef3806b52cd49b752b3e
sarah elkholy <saraelkholyarmy03@gmail.com>
2026-06-18 00:11:41 +0300

    - finished onboarding logic

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -5,4 +5,7 @@ abstract class AppAssets {
   static const String appLogo = "$_baseIconsPath/app_logo.svg";
 
   static const String testImage = "$_baseImagesPath/Image_test.png";
+
+  static const String onboardingAnimationImage =
+      "$_baseImagesPath/onboarding_animation.json";
 }
--- lib/features/auth/presentation/pages/onboarding/onboarding_screen.dart
+++ lib/features/auth/presentation/pages/onboarding/onboarding_screen.dart
@@ -1,83 +1,92 @@
+import 'package:flower_driver/core/helpers/my_responsive.dart';
+import 'package:flower_driver/core/localization/l10n/app_localizations.dart';
 import 'package:flower_driver/core/shared_widgets/custom_button.dart';
+import 'package:flower_driver/core/utils/app_assets.dart';
 import 'package:flower_driver/core/utils/app_colors.dart';
 import 'package:flower_driver/core/utils/app_text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+// بنستدعي الـ imports الخاصة بالـ Cache والـ Routes
+import 'package:flower_driver/config/di/di.dart';
+import 'package:flower_driver/config/route_manager/routes.dart';
+import 'package:flower_driver/config/secure_cache/secure_cache/cache_keys.dart';
+import 'package:flower_driver/config/secure_cache/secure_cache/secure_cache.dart';
+
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
+  Future<void> _completeOnboarding(
+    BuildContext context,
+    String targetRoute,
+  ) async {
+    final secureCache = getIt<SecureCache>();
+    await secureCache.saveData(key: CacheKeys.hasSeenOnboarding, value: 'true');
+
+    if (!context.mounted) return;
+    Navigator.pushReplacementNamed(context, targetRoute);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context)!;
     return Scaffold(
       backgroundColor: AppColors.baseWhite,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 50),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Spacer(flex: 2),
-
               SizedBox(
-                width: MediaQuery.of(context).size.height,
-                height: MediaQuery.of(context).size.height * 0.4,
+                width: MediaQuery.of(context).size.width * .75,
+                height: MediaQuery.of(context).size.height * 0.5,
                 child: OverflowBox(
                   minWidth: MediaQuery.of(context).size.height,
                   maxWidth: MediaQuery.of(context).size.height,
                   child: Center(
                     child: Lottie.asset(
-                      'assets/images/onboarding_animation.json',
+                      AppAssets.onboardingAnimationImage,
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
 
-              const Spacer(flex: 2),
-
               Text(
-                "Welcome to\nFlowery rider app",
-                style: AppTextStyles.medium20(context).copyWith(
-                  fontSize: 32,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
-                  height: 1.3,
-                ),
+                local.onboardingText,
+                style: AppTextStyles.medium20(context),
                 textAlign: TextAlign.start,
               ),
-
-              const Spacer(flex: 1),
+              SizedBox(height: 24),
 
               SizedBox(
                 width: double.infinity,
                 child: CustomButton(
-                  title: "Login",
+                  title: local.login,
                   onPressed: () {
-                    // Navigate to Login
+                    _completeOnboarding(context, Routes.loginRoute);
                   },
                 ),
               ),
 
-              const SizedBox(height: 16),
-
+              SizedBox(height: MyResponsive.fontSize(context, value: 16)),
               SizedBox(
                 width: double.infinity,
                 child: CustomButton(
-                  title: "Apply now",
-                  titleStyle: AppTextStyles.medium16(context).copyWith(
-                    color: AppColors.primaryColor,
-                    fontWeight: FontWeight.w500,
-                  ),
+                  title: local.applyNow,
+                  titleStyle: AppTextStyles.medium16(
+                    context,
+                  ).copyWith(color: AppColors.primaryColor),
                   backgroundColor: AppColors.baseWhite,
                   borderColor: AppColors.primaryColor,
                   onPressed: () {
-                    // Navigate to Apply screen
+                    _completeOnboarding(context, Routes.applyRoute);
                   },
                 ),
               ),
 
-              const Spacer(flex: 3),
+              const Spacer(),
 
               Center(
                 child: Text(
@@ -89,8 +98,6 @@ class OnboardingScreen extends StatelessWidget {
                   ),
                 ),
               ),
-
-              const SizedBox(height: 8),
             ],
           ),
         ),
