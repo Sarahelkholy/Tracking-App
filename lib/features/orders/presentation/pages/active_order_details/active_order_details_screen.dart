513777ca0635f71796758a2041c01cd08774357a
Mohamed Ali <mohamed.ali01537@gmail.com>
2026-07-10 00:24:12 +0300

    feat(active_order): refactor ActiveOrderDetailsScreen layout and improve code readability

--- lib/features/orders/presentation/pages/active_order_details/active_order_details_screen.dart
+++ lib/features/orders/presentation/pages/active_order_details/active_order_details_screen.dart
@@ -71,195 +71,202 @@ class _ActiveOrderDetailsScreenState extends State<ActiveOrderDetailsScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        key: const Key(KeysStrings.activeOrderAppBar),
-        title: Text(localizations.orderDetails),
-      ),
-      body: BlocBuilder<ActiveOrderCubit, ActiveOrderState>(
-        buildWhen: (previous, current) =>
-            previous.getActiveOrderState.isLoading !=
-                current.getActiveOrderState.isLoading ||
-            (previous.order == null && current.order != null),
-        builder: (context, state) {
-          if (state.getActiveOrderState.isLoading) {
-            return const CustomLoadingIndicator();
-          }
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          key: const Key(KeysStrings.activeOrderAppBar),
+          titleSpacing: 16,
+          automaticallyImplyLeading: false,
+          title: Text(localizations.orderDetails),
+        ),
+        body: BlocBuilder<ActiveOrderCubit, ActiveOrderState>(
+          buildWhen: (previous, current) =>
+          previous.getActiveOrderState.isLoading !=
+              current.getActiveOrderState.isLoading ||
+              (previous.order == null && current.order != null),
+          builder: (context, state) {
+            if (state.getActiveOrderState.isLoading) {
+              return const CustomLoadingIndicator();
+            }
 
-          final order = state.order;
+            final order = state.order;
 
-          if (order == null) {
-            return CustomErrorWidget(
-              errorMessage: localizations.noActiveOrderFound,
-            );
-          }
+            if (order == null) {
+              return CustomErrorWidget(
+                errorMessage: localizations.noActiveOrderFound,
+              );
+            }
 
-          return Column(
-            children: [
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: AppConstants.paddingHorizontal,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 16),
-                        BlocBuilder<ActiveOrderCubit, ActiveOrderState>(
-                          buildWhen: (p, c) =>
-                              p.order?.orderStatus != c.order?.orderStatus,
-                          builder: (context, state) {
-                            return Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                OrderStatusProgressBar(
-                                  key: const Key(
-                                    KeysStrings.activeOrderProgressBar,
+            return Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: AppConstants.paddingHorizontal,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 16),
+                          BlocBuilder<ActiveOrderCubit, ActiveOrderState>(
+                            buildWhen: (p, c) =>
+                            p.order?.orderStatus != c.order?.orderStatus,
+                            builder: (context, state) {
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  OrderStatusProgressBar(
+                                    key: const Key(
+                                      KeysStrings.activeOrderProgressBar,
+                                    ),
+                                    currentStep: state.order!.orderStatus.step,
                                   ),
-                                  currentStep: state.order!.orderStatus.step,
+                                  const SizedBox(height: 24),
+                                  OrderDetailsStatusSection(
+                                    key: const Key(
+                                      KeysStrings.activeOrderDetailsStatus,
+                                    ),
+                                    status: state.order!.orderStatus.localized(
+                                      localizations,
+                                    ),
+                                    orderId: state.order!.orderNumber,
+                                    orderTime: state.order!.createdAt,
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            localizations.pickupAddress,
+                            style: AppTextStyles.medium18(context),
+                          ),
+                          const SizedBox(height: 16),
+                          ContactAddressCard(
+                            key: const Key(
+                                KeysStrings.activeOrderPickupAddress),
+                            imageUrl: order.store.image,
+                            name: order.store.name,
+                            address: order.store.address,
+                            onCallPressed: () {
+                              getIt<UrlLauncherHelper>().callPhone(
+                                order.store.phoneNumber,
+                              );
+                            },
+                            onWhatsappPressed: () {
+                              getIt<UrlLauncherHelper>().launchWhatsApp(
+                                order.store.phoneNumber,
+                              );
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            localizations.userAddress,
+                            style: AppTextStyles.medium18(context),
+                          ),
+                          const SizedBox(height: 16),
+                          ContactAddressCard(
+                            key: const Key(KeysStrings.activeOrderUserAddress),
+                            imageUrl: order.user.photo,
+                            name:
+                            "${order.user.firstName} ${order.user.lastName}",
+                            address:
+                            "${order.shippingAddress.street}, ${order
+                                .shippingAddress.city}",
+                            onCallPressed: () {
+                              getIt<UrlLauncherHelper>().callPhone(
+                                order.user.phone,
+                              );
+                            },
+                            onWhatsappPressed: () {
+                              getIt<UrlLauncherHelper>().launchWhatsApp(
+                                order.user.phone,
+                              );
+                            },
+                          ),
+                          const SizedBox(height: 16),
+                          Text(
+                            localizations.orderDetails,
+                            style: AppTextStyles.medium18(context),
+                          ),
+                          const SizedBox(height: 8),
+                          Column(
+                            key: const Key(KeysStrings.activeOrderItemsList),
+                            children: List.generate(
+                                order.orderItems.length, (index,) {
+                              final item = order.orderItems[index];
+                              return Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: OrderDetailsItem(
+                                  imageUrl: item.product.imgCover,
+                                  title: item.product.title,
+                                  price: item.price.toString(),
+                                  numberOfItem: item.quantity.toInt(),
                                 ),
-                                const SizedBox(height: 24),
-                                OrderDetailsStatusSection(
-                                  key: const Key(
-                                    KeysStrings.activeOrderDetailsStatus,
-                                  ),
-                                  status: state.order!.orderStatus.localized(
-                                    localizations,
-                                  ),
-                                  orderId: state.order!.orderNumber,
-                                  orderTime: state.order!.createdAt,
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          localizations.pickupAddress,
-                          style: AppTextStyles.medium18(context),
-                        ),
-                        const SizedBox(height: 16),
-                        ContactAddressCard(
-                          key: const Key(KeysStrings.activeOrderPickupAddress),
-                          imageUrl: order.store.image,
-                          name: order.store.name,
-                          address: order.store.address,
-                          onCallPressed: () {
-                            getIt<UrlLauncherHelper>().callPhone(
-                              order.store.phoneNumber,
-                            );
-                          },
-                          onWhatsappPressed: () {
-                            getIt<UrlLauncherHelper>().launchWhatsApp(
-                              order.store.phoneNumber,
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          localizations.userAddress,
-                          style: AppTextStyles.medium18(context),
-                        ),
-                        const SizedBox(height: 16),
-                        ContactAddressCard(
-                          key: const Key(KeysStrings.activeOrderUserAddress),
-                          imageUrl: order.user.photo,
-                          name:
-                              "${order.user.firstName} ${order.user.lastName}",
-                          address:
-                              "${order.shippingAddress.street}, ${order.shippingAddress.city}",
-                          onCallPressed: () {
-                            getIt<UrlLauncherHelper>().callPhone(
-                              order.user.phone,
-                            );
-                          },
-                          onWhatsappPressed: () {
-                            getIt<UrlLauncherHelper>().launchWhatsApp(
-                              order.user.phone,
-                            );
-                          },
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          localizations.orderDetails,
-                          style: AppTextStyles.medium18(context),
-                        ),
-                        const SizedBox(height: 8),
-                        Column(
-                          key: const Key(KeysStrings.activeOrderItemsList),
-                          children: List.generate(order.orderItems.length, (
-                            index,
-                          ) {
-                            final item = order.orderItems[index];
-                            return Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: OrderDetailsItem(
-                                imageUrl: item.product.imgCover,
-                                title: item.product.title,
-                                price: item.price.toString(),
-                                numberOfItem: item.quantity.toInt(),
-                              ),
-                            );
-                          }),
-                        ),
-                        const SizedBox(height: 24),
-                        OrderDetailsRowText(
-                          key: const Key(KeysStrings.activeOrderTotal),
-                          title: localizations.total,
-                          value: "${localizations.egp} ${order.totalPrice}",
-                        ),
-                        const SizedBox(height: 24),
-                        OrderDetailsRowText(
-                          key: const Key(KeysStrings.activeOrderPaymentMethod),
-                          title: localizations.paymentMethod,
-                          value: order.paymentType,
-                        ),
-                        const SizedBox(height: 16),
-                      ],
+                              );
+                            }),
+                          ),
+                          const SizedBox(height: 24),
+                          OrderDetailsRowText(
+                            key: const Key(KeysStrings.activeOrderTotal),
+                            title: localizations.total,
+                            value: "${localizations.egp} ${order.totalPrice}",
+                          ),
+                          const SizedBox(height: 24),
+                          OrderDetailsRowText(
+                            key: const Key(
+                                KeysStrings.activeOrderPaymentMethod),
+                            title: localizations.paymentMethod,
+                            value: order.paymentType,
+                          ),
+                          const SizedBox(height: 16),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              BlocBuilder<ActiveOrderCubit, ActiveOrderState>(
-                buildWhen: (p, c) =>
-                    p.order?.orderStatus != c.order?.orderStatus ||
-                    p.updateOrderStatusState.isLoading !=
-                        c.updateOrderStatusState.isLoading,
-                builder: (context, state) {
-                  return Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 24,
-                    ),
-                    decoration: BoxDecoration(
-                      color: AppColors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.black100.withValues(alpha: 0.2),
-                          blurRadius: 4,
-                          offset: const Offset(0, -2),
-                        ),
-                      ],
-                    ),
-                    child: CustomButton(
-                      key: const Key(KeysStrings.activeOrderButton),
-                      isLoading: state.updateOrderStatusState.isLoading,
-                      title: state.order!.orderStatus.buttonTitle(
-                        localizations,
+                BlocBuilder<ActiveOrderCubit, ActiveOrderState>(
+                  buildWhen: (p, c) =>
+                  p.order?.orderStatus != c.order?.orderStatus ||
+                      p.updateOrderStatusState.isLoading !=
+                          c.updateOrderStatusState.isLoading,
+                  builder: (context, state) {
+                    return Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 24,
                       ),
-                      onPressed:
-                          state.order!.orderStatus == OrderStatusEnum.delivered
-                          ? null
-                          : () => _onButtonPressed(context, state.order!),
-                    ),
-                  );
-                },
-              ),
-            ],
-          );
-        },
+                      decoration: BoxDecoration(
+                        color: AppColors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColors.black100.withValues(alpha: 0.2),
+                            blurRadius: 4,
+                            offset: const Offset(0, -2),
+                          ),
+                        ],
+                      ),
+                      child: CustomButton(
+                        key: const Key(KeysStrings.activeOrderButton),
+                        isLoading: state.updateOrderStatusState.isLoading,
+                        title: state.order!.orderStatus.buttonTitle(
+                          localizations,
+                        ),
+                        onPressed:
+                        state.order!.orderStatus == OrderStatusEnum.delivered
+                            ? null
+                            : () => _onButtonPressed(context, state.order!),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
