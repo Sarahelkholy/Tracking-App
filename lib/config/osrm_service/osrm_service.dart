63a9ee37ebe41f0c8f959ded52e99dac1d68508e
Rahma Ashraf <rahmaashraf.dev@gmail.com>
2026-07-10 22:33:25 +0300

    fix: update path for secret_keys.dart in .gitignore

--- lib/config/osrm_service/osrm_service.dart
+++ lib/config/osrm_service/osrm_service.dart
@@ -5,7 +5,7 @@ import 'package:latlong2/latlong.dart';
 class OsrmService {
   // TODO: Replace with your actual OpenRouteService API Key
   final OpenRouteService client = OpenRouteService(
-    apiKey: SecretKeys.openRouteService,
+    apiKey: SecretKeys.openRouteServiceApiKey,
   );
 
   Future<List<LatLng>> getRoute({
